9fc6e810cc92007a11e92b12f491d6286bfb13b8
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-10-23 15:18:12 +0300

    task3/fix: change path to scss dir

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-    return gulp.src(['src/task3/scss/*.scss'])
+    return gulp.src(['src/task3/scss/style.scss'])
     		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
    			.pipe(gulp.dest('src/task3/css'));
 })
 
 gulp.task('watch', function(){
-    gulp.watch(['src/task3/scss/*.scss'], ['sass']);
+    gulp.watch(['src/task3/scss/style.scss'], ['sass']);
 })
 
 gulp.task('default', ['sass', 'watch']);
