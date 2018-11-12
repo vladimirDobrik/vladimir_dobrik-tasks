3a789a5003375b46043a64c70ffe76529ec04f44
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-11-12 20:00:59 +0300

    js-task11/feat: display input for set date

--- src/js/task11/index.js
+++ src/js/task11/index.js
@@ -33,6 +33,7 @@
 
             CustomCalendarProto.createdCallback = function () {
                 this.innerHTML = '<style></style><table><thead><tr><td></td><td colspan="4"></td><td></td><td></td></tr>\
+                <tr><td colspan="7"></td></tr>\
                 <tr><td>M</td><td>T</td><td>W</td><td>T</td><td>F</td><td>S</td><td>S</td></tr>\
                 </thead><tbody></tbody></table>';
             };
@@ -68,6 +69,9 @@
 
             document.querySelector('table thead tr:last-child td:nth-of-type(6)').style.cssText = "\
             color: red;";
+
+            document.querySelector('table thead tr:nth-of-type(2) td:first-child').innerHTML = "\
+            <input>";
 
             (function CustomCalendar(year, month, day) {
 
@@ -144,6 +148,30 @@
                         }
                     }
                 }
+
+                var input = document.querySelector('table thead tr:nth-of-type(2) td:first-child input');
+
+                var innerInput = new Date().toLocaleString('en', {
+                    day: 'numeric',
+                    year: 'numeric',
+                    month: 'numeric',
+                });
+                input.setAttribute('type', 'text');
+                input.setAttribute('id', 'search-date');
+                input.setAttribute('data-mask', "__/__/____");
+                input.setAttribute('placeholder', innerInput);
+                input.style.cssText = "\
+                    display: inline-block;\
+                    padding: 6px 15px;\
+                    text-decoration: none;\
+                    border: 2px solid #96deda;\
+                    font-weight: 800;\
+                    font-size: 16px;\
+                    color: #000;\
+                    border-radius: 20px;\
+                    width: 100px;\
+                    outline: none;\
+                    background-color: #fff;";
 
                 var tHeader = document.querySelector('table thead td:nth-of-type(2)');
                 var tBody = document.querySelector('tbody');
@@ -250,6 +278,53 @@
 
                     e.stopImmediatePropagation();
                 })
+
+                applyDataMask(input);
+
+                function applyDataMask(field) {
+                    var mask = field.dataset.mask.split('');
+
+                    function stripMask(maskedData) {
+                        function isDigit(char) {
+                            return /\d/.test(char);
+                        }
+                        return maskedData.split('').filter(isDigit);
+                    }
+
+                    function applyMask(data) {
+                        return mask.map(function (char) {
+                            if (char !== '_') return char;
+                            if (data.length == 0) return char;
+                            return data.shift();
+                        }).join('')
+                    }
+
+                    function reapplyMask(data) {
+                        return applyMask(stripMask(data));
+                    }
+
+                    function changed(e) {
+                        e = e || event;
+                        var oldStart = field.selectionStart;
+                        var oldEnd = field.selectionEnd;
+
+                        field.value = reapplyMask(field.value);
+
+                        field.selectionStart = oldStart;
+                        field.selectionEnd = oldEnd;
+
+                        if (e.keyCode === 13) {
+                            var exportDate = String(field.value).split('/').map(function (elem) {
+                                return Number(elem);
+                            });
+                            console.log(exportDate);
+                            CustomCalendar(exportDate[2], exportDate[1] - 1, exportDate[0]);
+                        }
+                        e.stopImmediatePropagation();
+                    }
+
+                    field.addEventListener('keyup', changed);
+                }
             })();
 
             document.querySelector('#run-btn').style.display = 'none';
