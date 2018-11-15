8dffbe5d834851da71e18434497c852d6a699a38
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-11-15 16:44:43 +0300

    task_1-task1: update code style

---
+++ src/js/task_1/task1/index.js
@@ -0,0 +1,27 @@
+let isSimpleNumber = num => {
+
+    let result = true;
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+
+let getPrimeNumbers = num => {
+
+    const result = [];
+
+    for (let i = 2; i < num; i++) {
+        if (isSimpleNumber(i))
+            result.push(i);
+    }
+
+    return result;
+};
+
+module.exports = getPrimeNumbers;
