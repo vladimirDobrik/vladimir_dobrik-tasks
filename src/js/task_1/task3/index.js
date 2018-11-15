60391f8c3a5ccf7e1293456a4424f2c7a6eaba51
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-11-15 16:45:06 +0300

    task_1-task3: update code style

---
+++ src/js/task_1/task3/index.js
@@ -0,0 +1,36 @@
+const getSumFib = num => {
+
+    let temp = [];
+    let result;
+
+    const getFib = num => {
+
+        let result = 0;
+
+        if (num < 2) {
+            return num;
+        } else {
+            result = getFib(num - 1) + getFib(num - 2);
+            return result;
+        }
+    };
+
+    let i = 2;
+
+    while (temp.length < num) {
+
+        let fibNum = getFib(i);
+
+        if (fibNum % 2 === 0) {
+            temp.push(fibNum);
+        }
+
+        i++;
+    }
+
+    result = temp.reduce((acc, cur) => acc + cur);
+
+    return result;
+}
+
+module.exports = getSumFib;
