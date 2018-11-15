db52bad069bb5d8988a85e73795e279a1fbf51e1
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-11-15 16:27:34 +0300

    js-task/delete: delete old files

--- src/js/task3/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = getSum = (str) => {
-
-    let result = String(str).split('')
-                            .map((elem) => elem = +elem)
-                            .reduce((acc, cur) => acc + cur);
-
-    return result;
-}
--- src/js/task8/test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const getLastTenDigits = require('./index');
-
-describe('get last 10 digits', () => {
-    it('should return last 10 digits', () => {
-        assert.equal(getLastTenDigits(10), 405071317);
-    });
-})
