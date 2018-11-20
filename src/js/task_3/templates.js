acf6338ee5a67894fc9c3da3049a4d173e45d306
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-11-20 12:08:48 +0300

    task_3/refactor: refactor customCalendar func

---
+++ src/js/task_3/templates.js
@@ -0,0 +1,74 @@
+var DatepickerTemplates = {};
+
+DatepickerTemplates.calendarStructureTemplate =
+    '<table><thead>\
+    <tr><td></td><td colspan="4"></td><td></td><td></td></tr>\
+    <tr><td colspan="7"><input></td></tr>\
+    <tr><td>M</td><td>T</td><td>W</td><td>T</td><td>F</td><td>S</td><td>S</td></tr>\
+    </thead><tbody></tbody></table>';
+
+DatepickerTemplates.runButtonStyle = "\
+    text-transform: uppercase;\
+    display: inline-block;\
+    padding: 6px 10px;\
+    text-align: center;\
+    text-decoration: none;\
+    border: 2px solid #96deda;\
+    font-weight: 800;\
+    font-size: 16px;\
+    color: #000;\
+    border-radius: 20px;\
+    width: 170px;\
+    outline: none;\
+    background-color: transparent;\
+    cursor: pointer;";
+
+DatepickerTemplates.closeButtonStyle = "\
+    cursor: pointer;"
+
+DatepickerTemplates.previousButtonStyle = "\
+    cursor: pointer;"
+
+DatepickerTemplates.nextButtonStyle = "\
+    cursor: pointer;"
+
+DatepickerTemplates.tableStyle = "\
+    font-size: 20px;\
+    background-color: #96deda;\
+    padding: 10px;\
+    text-align: center;\
+    cursor: default;\
+    border-radius: 10px;\
+    font-family: sans-serif;";
+
+DatepickerTemplates.tableHeadStyle = "\
+    font-size: 30px;\
+    background-color: #96deda;\
+    text-align: center;";
+
+DatepickerTemplates.tableHeaderStyle = "\
+    color: #fff;";
+
+DatepickerTemplates.tableBodyStyle = "\
+    font-size: 30px;\
+    background-color: #D7FFFE;\
+    text-align: center;";
+
+DatepickerTemplates.tableHeadWeekendStyle = "\
+    color: red;";
+
+DatepickerTemplates.dateInputStyle = "\
+    display: inline-block;\
+    padding: 6px 15px;\
+    text-decoration: none;\
+    border: 2px solid #96deda;\
+    font-weight: 800;\
+    font-size: 16px;\
+    color: #000;\
+    border-radius: 20px;\
+    width: 100px;\
+    outline: none;\
+    background-color: #fff;";
+
+
+
