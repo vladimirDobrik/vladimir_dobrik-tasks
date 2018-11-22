1be39bde1918957e0664c307741fcd3e9509a4b3
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-11-23 01:25:16 +0300

    task_3: refactor drawCalendar func add renderElem func

--- src/js/task_3/index.js
+++ src/js/task_3/index.js
@@ -10,28 +10,49 @@
 
         var datepickerContainer = createDatepickerContainer();
         var tableContainer = createTableContainer();
-        var btn = createRunBtn();
+        var runbtn = createRunBtn();
         var todayBtn = createTodayBtn();
         var dateInput = createDateInput();
 
+        applyStyles();
         registerComponent();
-        initComponentsStyles();
         initEvents();
         applyDataMask(dateInput);
 
         function createDatepickerContainer() {
-
             var datepickerContainer = document.createElement('custom-calendar');
+
+            datepickerContainer.className = 'custom-calendar';
+
             document.getElementById(containerID).appendChild(datepickerContainer);
 
             return datepickerContainer;
         }
 
+        function applyStyles() {
+            var style = document.createElement('style');
+
+            style.type = 'text/css';
+            style.innerHTML = DatepickerTemplates.styles;
+
+            var prevMonth = document.querySelector('#preMonth');
+            var nextMonth = document.querySelector('#nextMonth');
+            var close = document.querySelector('#close');
+
+            prevMonth.innerHTML = '<i class="fas fa-angle-double-left"></i>';
+            nextMonth.innerHTML = '<i class="fas fa-angle-double-right"></i>';
+            close.innerHTML = '<i class="far fa-times-circle"></i>';
+
+            datepickerContainer.insertBefore(style, tableContainer);
+        }
+
         function createTableContainer() {
 
             var tableContainer = document.createElement('div');
+
             datepickerContainer.appendChild(tableContainer);
             tableContainer.style.display = 'none';
+            tableContainer.className = 'custom-calendar__content';
             tableContainer.innerHTML = DatepickerTemplates.calendarStructureTemplate;
 
             return tableContainer;
@@ -39,51 +60,28 @@
 
         function createRunBtn() {
 
-            var btn = document.createElement('button');
-            
-            btn.innerHTML = 'calendar <i class="far fa-calendar-alt"></i>';
-            btn.setAttribute('id', 'run-btn');
-            btn.style.cssText = DatepickerTemplates.runButtonStyle;
+            var runbtn = document.createElement('button');
 
-            datepickerContainer.appendChild(btn);
+            runbtn.innerHTML = 'calendar <i class="far fa-calendar-alt"></i>';
+            runbtn.id = 'run-btn';
+            runbtn.className = 'custom-calendar__btn';
 
-            return btn;
+            datepickerContainer.appendChild(runbtn);
+
+            return runbtn;
         }
 
         function createTodayBtn() {
 
             var todayBtn = document.createElement('button');
+
             todayBtn.innerText = 'today';
-            todayBtn.setAttribute('id', 'today-btn');
-            todayBtn.style.cssText = DatepickerTemplates.todayButtontyle;
+            todayBtn.id = 'today-btn';
+            todayBtn.classList.add('custom-calendar__btn', 'custom-calendar__btn--today');
 
             document.querySelector('table thead tr:nth-of-type(2) td:first-child').appendChild(todayBtn);
 
             return todayBtn;
-        }
-
-        function initComponentsStyles() {
-
-            document.querySelector('table').style.cssText = DatepickerTemplates.tableStyle;
-            document.querySelector('thead').style.cssText = DatepickerTemplates.tableHeadStyle;
-            document.querySelector('tbody').style.cssText = DatepickerTemplates.tableBodyStyle;
-            document.querySelector('table thead tr:last-child td:last-child')
-                .style.cssText = DatepickerTemplates.tableHeadWeekendStyle;
-            document.querySelector('table thead tr:last-child td:nth-of-type(6)')
-                .style.cssText = DatepickerTemplates.tableHeadWeekendStyle;
-
-            var prevMonth = document.querySelector('thead tr:nth-of-type(1) td:nth-of-type(1)');
-            var nextMonth = document.querySelector('thead tr:nth-of-type(1) td:nth-of-type(3)');
-            var close = document.querySelector('thead tr:nth-of-type(1) td:nth-of-type(4)');
-            var tHeader = document.querySelector('table thead td:nth-of-type(2)');
-
-            tHeader.style.cssText = DatepickerTemplates.tableHeaderStyle;
-            close.style.cssText = DatepickerTemplates.closeButtonStyle;
-            prevMonth.style.cssText = DatepickerTemplates.previousButtonStyle;
-            nextMonth.style.cssText = DatepickerTemplates.nextButtonStyle;
-            prevMonth.innerHTML = '<i class="fas fa-angle-double-left"></i>';
-            nextMonth.innerHTML = '<i class="fas fa-angle-double-right"></i>';
-            close.innerHTML = '<i class="far fa-times-circle"></i>';
         }
 
         function getFormattedDate(year, month, day) {
@@ -99,36 +97,10 @@
 
         function initEvents() {
 
-            var prevMonth = document.querySelector('thead tr:nth-of-type(1) td:nth-of-type(1)');
-            var nextMonth = document.querySelector('thead tr:nth-of-type(1) td:nth-of-type(3)');
-            var close = document.querySelector('thead tr:nth-of-type(1) td:nth-of-type(4)');
-            var tbody = document.querySelector('tbody');
-
-            close.onmouseover = function (e) {
-                e = e || event;
-                event.target.style.color = '#fff';
-            }
-
-            close.onmouseout = function (e) {
-                e = e || event;
-                event.target.style.color = '#000';
-            }
-
-            prevMonth.onmouseover = function (e) {
-                e.target.style.color = '#fff';
-            }
-
-            prevMonth.onmouseout = function (e) {
-                e.target.style.color = '#000';
-            }
-
-            nextMonth.onmouseover = function (e) {
-                e.target.style.color = '#fff';
-            }
-
-            nextMonth.onmouseout = function (e) {
-                e.target.style.color = '#000';
-            }
+            var prevMonth = document.querySelector('#preMonth');
+            var nextMonth = document.querySelector('#nextMonth');
+            var close = document.querySelector('#close');
+            var tbody = document.querySelector('.custom-calendar__days');
 
             prevMonth.addEventListener('click', function (e) {
                 e = e || event;
@@ -149,11 +121,11 @@
             close.addEventListener('click', function (e) {
                 e = e || event;
                 tableContainer.style.display = 'none';
-                btn.style.display = '';
+                runbtn.style.display = '';
                 e.stopImmediatePropagation();
             });
 
-            btn.addEventListener('click', function (e) {
+            runbtn.addEventListener('click', function (e) {
                 this.style.display = 'none';
                 drawCalendar();
                 tableContainer.style.display = '';
@@ -169,8 +141,8 @@
 
             tbody.addEventListener('click', function (e) {
                 var day = e.target.innerText;
-                var isPreviousMonth = e.target.getAttribute('data-isPrevious');
-                var isNextMonth = e.target.getAttribute('data-isNext');
+                var isPreviousMonth = e.target.getAttribute('data-is-previous');
+                var isNextMonth = e.target.getAttribute('data-is-next');
                 var increment = isPreviousMonth ? -1 : isNextMonth ? 1 : 0;
 
                 displayingMonth = displayingMonth + increment;
@@ -196,14 +168,13 @@
 
         function createDateInput() {
 
-            var dateInput = document.querySelector('table thead tr:nth-of-type(2) td:first-child input');
+            var dateInput = document.querySelector('.custom-calendar__input');
             var innerInput = getFormattedDate(selectedYear, selectedMonth, selectedDay);
 
-            dateInput.setAttribute('type', 'text');
-            dateInput.setAttribute('id', 'search-date');
-            dateInput.setAttribute('data-mask', "__/__/____");
-            dateInput.setAttribute('value', innerInput);
-            dateInput.style.cssText = DatepickerTemplates.dateInputStyle;
+            dateInput.type = 'text';
+            dateInput.id = 'datepicker';
+            dateInput.dataset.mask = "__/__/____";
+            dateInput.value = innerInput;
 
             return dateInput;
         }
@@ -212,7 +183,7 @@
 
             var CustomCalendarProto = Object.create(HTMLElement.prototype);
 
-            var CustomCalendar = document.registerElement('custom-calendar', {
+            document.registerElement('custom-calendar', {
                 prototype: CustomCalendarProto
             });
         }
@@ -222,13 +193,18 @@
             var lastDayOfMonth = new Date(displayingYear, displayingMonth + 1, 0).getDate();
             var firstDayOfWeekOfCurMonth = new Date(displayingYear, displayingMonth, 1).getDay();
             var lastDayOfWeekOfCurMonth = new Date(displayingYear, displayingMonth + 1, 0).getDay();
-            var innerTBody = '';
+
+            document.querySelector('.custom-calendar__days').innerHTML = '';
 
             if (firstDayOfWeekOfCurMonth !== 0) {
                 for (var dayOfWeek = 1; dayOfWeek < firstDayOfWeekOfCurMonth; dayOfWeek++) {
                     var dayOfPreMonth = new Date(displayingYear, displayingMonth, (-(firstDayOfWeekOfCurMonth - 1) + dayOfWeek)).getDate();
-                    innerTBody += '<td data-isPrevious="true" style="color:rgba(0, 0, 0, .3); padding: 10px;">' +
-                        dayOfPreMonth + '</td>';
+                    var classes = [
+                        'custom-calendar__day',
+                        'custom-calendar__day--other-month'
+                    ];
+
+                    renderElem('td', classes, '.custom-calendar__days', 'isPrevious');
                 }
             } else {
                 for (var dayOfWeek = 5; dayOfWeek >= 0; dayOfWeek--) {
@@ -237,11 +213,20 @@
                     var dayOfWeekOfPreMonth = preDate.getDay();
 
                     if (dayOfWeekOfPreMonth > 5 || dayOfWeekOfPreMonth === 0) {
-                        innerTBody += '<td data-isPrevious="true" style="color:rgba(255, 0, 0, .3); padding: 10px;">' +
-                            dayOfPreMonth + '</td>';
+                        var classes = [
+                            'custom-calendar__day',
+                            'custom-calendar__day--other-month',
+                            'custom-calendar__day--day-off'
+                        ];
+
+                        renderElem('td', classes, '.custom-calendar__days', 'isPrevious');
                     } else {
-                        innerTBody += '<td data-isPrevious="true" style="color:rgba(0, 0, 0, .3); padding: 10px;">' +
-                            dayOfPreMonth + '</td>';
+                        var classes = [
+                            'custom-calendar__day',
+                            'custom-calendar__day--other-month'
+                        ];
+
+                        renderElem('td', classes, '.custom-calendar__days', 'isPrevious');
                     }
                 }
             }
@@ -254,29 +239,49 @@
                     day == selectedDay) {
 
                     if (dayOfWeek > 0 && dayOfWeek < 6) {
-                        innerTBody += '<td style="border-radius: 50%;">' +
-                            day + '</td>';
+                        var classes = [
+                            'custom-calendar__day',
+                            'custom-calendar__day--selected'
+                        ];
+
+                        renderElem('td', classes, '.custom-calendar__days');
                     } else {
-                        innerTBody += '<td style="color:rgb(255, 0, 0); padding: 10px; border-radius: 50%;">' +
-                            day + '</td>';
+                        var classes = [
+                            'custom-calendar__day',
+                            'custom-calendar__day--selected',
+                            'custom-calendar__day--day-off'
+                        ];
+
+                        renderElem('td', classes, '.custom-calendar__days');
                     }
 
                     if (dayOfWeek === 0) {
-                        innerTBody += '<tr>';
+                        var tr = document.createElement('tr');
+                        var parent = document.querySelector('.custom-calendar__days');
+                        parent.insertBefore(tr, parent.nextElementSibling);
                     }
                     continue;
                 }
 
                 if (dayOfWeek > 0 && dayOfWeek < 6) {
-                    innerTBody += '<td style="padding: 10px;">' +
-                        day + '</td>';
+                    var classes = [
+                        'custom-calendar__day'
+                    ];
+
+                    renderElem('td', classes, '.custom-calendar__days');
                 } else {
-                    innerTBody += '<td style="color:rgb(255, 0, 0); padding: 10px;">' +
-                        day + '</td>';
+                    var classes = [
+                        'custom-calendar__day',
+                        'custom-calendar__day--day-off'
+                    ];
+
+                    renderElem('td', classes, '.custom-calendar__days');
                 }
 
                 if (dayOfWeek === 0) {
-                    innerTBody += '<tr>';
+                    var tr = document.createElement('tr');
+                    var parent = document.querySelector('.custom-calendar__days');
+                    parent.insertBefore(tr, parent.nextElementSibling);
                 }
             }
 
@@ -287,11 +292,21 @@
                     var dayOfWeekOfNextMonth = nextDate.getDay();
 
                     if (dayOfWeekOfNextMonth > 5 || dayOfWeekOfNextMonth === 0) {
-                        innerTBody += '<td data-isNext="true" style="color:rgba(255, 0, 0, .3); padding: 10px;">' +
-                            dayOfNextMonth + '</td>';
+                        var classes = [
+                            'custom-calendar__day',
+                            'custom-calendar__day--other-month',
+                            'custom-calendar__day--day-off'
+                        ];
+
+                        renderElem('td', classes, '.custom-calendar__days', 'isNext');
+
                     } else {
-                        innerTBody += '<td data-isNext="true" style="color:rgba(0, 0, 0, .3); padding: 10px;">' +
-                            dayOfNextMonth + '</td>';
+                        var classes = [
+                            'custom-calendar__day',
+                            'custom-calendar__day--other-month'
+                        ];
+                        
+                        renderElem('td', classes, '.custom-calendar__days', 'isNext');
                     }
                 }
             }
@@ -303,7 +318,28 @@
                 year: 'numeric'
             });
 
-            document.querySelector('tbody').innerHTML = innerTBody;
+            function renderElem(elem, classes, parent, dataAttr) {
+                
+                var child = document.createElement(elem);
+                var parent = document.querySelector(parent);
+
+                if(dataAttr) {
+                    if(dataAttr === 'isPrevious') {
+                        child.innerText = dayOfPreMonth;
+                        child.dataset[dataAttr] = "true";
+
+                    } else if(dataAttr === 'isNext') {
+                        child.innerText = dayOfNextMonth;
+                        child.dataset[dataAttr] = "true";
+                    }
+                } else {
+                    child.innerText = day;
+                }
+
+                child.classList.add(...classes);
+
+                parent.insertBefore(child, parent.nextElementSibling);
+            }
         }
 
         function applyDataMask(field) {
--- src/js/task_3/templates.js
+++ src/js/task_3/templates.js
@@ -1,113 +1,128 @@
 var DatepickerTemplates = {};
 
+DatepickerTemplates.styles = "\
+    .custom-calendar__content {\
+        display: inline-block;\
+    }\
+    .custom-calendar__btn {\
+        text-transform: uppercase;\
+        display: inline-block;\
+        padding: 6px 10px;\
+        text-align: center;\
+        text-decoration: none;\
+        border: 2px solid #96deda;\
+        font-weight: 800;\
+        font-size: 14px;\
+        color: #000;\
+        border-radius: 20px;\
+        width: 150px;\
+        outline: none;\
+        background-color: transparent;\
+        cursor: pointer;\
+    }\
+    .custom-calendar__btn--today {\
+        font-weight: 600;\
+        width: 100px;\
+        background-color: #fff;\
+    }\
+    .custom-calendar__container {\
+        font-size: 15px;\
+        background-color: #96deda;\
+        padding: 10px;\
+        text-align: center;\
+        cursor: default;\
+        border-radius: 10px;\
+        font-family: sans-serif;\
+        user-select: none;\
+    }\
+    .custom-calendar__header {\
+        background-color: #96deda;\
+    }\
+    .custom-calendar__control-panel {\
+        font-size: 20px;\
+    }\
+    .control-panel__switch {\
+        cursor: pointer;\
+        color: #000;\
+    }\
+    .control-panel__switch:hover {\
+        color: #fff;\
+        transform: scale(1.5);\
+    }\
+    .control-panel__status {\
+        color: #fff;\
+    }\
+    .custom-calendar__input {\
+        padding: 6px 15px;\
+        border: 0;\
+        font-weight: 600;\
+        font-size: 14px;\
+        color: #000;\
+        border-radius: 20px;\
+        width: 100px;\
+        outline: none;\
+        letter-spacing: 1.5px;\
+        text-align: center;\
+        margin-right: 50px;\
+    }\
+    .custom-calendar__weekdays {\
+        font-size: 11px;\
+        letter-spacing: 2px;\
+    }\
+    .custom-calendar__weekday:first-letter {\
+        font-size: 14px;\
+        font-weight: 600;\
+    }\
+    .custom-calendar__weekday--dayOff {\
+        color: red;\
+    }\
+    .custom-calendar__days {\
+        font-size: 18px;\
+        background-color: #D7FFFE;\
+        letter-spacing: 1px;\
+    }\
+    .custom-calendar__day {\
+        padding: 10px;\
+        font-size: 20px;\
+    }\
+    .custom-calendar__day--other-month {\
+        opacity: 0.3;\
+    }\
+    .custom-calendar__day--day-off {\
+        color: red;\
+    }\
+    .custom-calendar__day--selected {\
+        border-radius: 50%;\
+        background-color: #fff;\
+    }";
+
 DatepickerTemplates.calendarStructureTemplate =
-    '<table>\
-        <thead>\
-            <tr>\
-                <td></td>\
-                <td colspan="4"></td>\
-                <td></td>\
-                <td></td>\
+    '<table class="custom-calendar__container">\
+        <thead class="custom-calendar__header">\
+            <tr class="custom-calendar__control-panel control-panel">\
+                <td class="control-panel__switch" id="preMonth"></td>\
+                <td class="control-panel__status" colspan="4"></td>\
+                <td class="control-panel__switch" id="nextMonth"></td>\
+                <td class="control-panel__switch" id="close"></td>\
             </tr>\
-            <tr>\
-                <td colspan="7">\
-                    <input>\
+            <tr class="custom-calendar__datepicker">\
+                <td class="custom-calendar__datepicker-container" colspan="7">\
+                    <input class="custom-calendar__input">\
                 </td>\
             </tr>\
-            <tr>\
-                <td>Mon</td>\
-                <td>Tue</td>\
-                <td>Wed</td>\
-                <td>Thu</td>\
-                <td>Fri</td>\
-                <td>Sat</td>\
-                <td>Sun</td>\
+            <tr class="custom-calendar__weekdays">\
+                <td class="custom-calendar__weekday">Mon</td>\
+                <td class="custom-calendar__weekday">Tue</td>\
+                <td class="custom-calendar__weekday">Wed</td>\
+                <td class="custom-calendar__weekday">Thu</td>\
+                <td class="custom-calendar__weekday">Fri</td>\
+                <td class="custom-calendar__weekday custom-calendar__weekday--dayOff">Sat</td>\
+                <td class="custom-calendar__weekday custom-calendar__weekday--dayOff">Sun</td>\
             </tr>\
         </thead>\
-        <tbody></tbody>\
+        <tbody class="custom-calendar__days"></tbody>\
     </table>';
 
-DatepickerTemplates.runButtonStyle = "\
-    text-transform: uppercase;\
-    display: inline-block;\
-    padding: 6px 10px;\
-    text-align: center;\
-    text-decoration: none;\
-    border: 2px solid #96deda;\
-    font-weight: 800;\
-    font-size: 14px;\
-    color: #000;\
-    border-radius: 20px;\
-    width: 150px;\
-    outline: none;\
-    background-color: transparent;\
-    cursor: pointer;";
-
-DatepickerTemplates.todayButtontyle = "\
-    text-transform: uppercase;\
-    display: inline-block;\
-    padding: 6px 10px;\
-    text-align: center;\
-    text-decoration: none;\
-    border: 2px solid #96deda;\
-    font-weight: 600;\
-    font-size: 14px;\
-    color: #000;\
-    border-radius: 20px;\
-    width: 100px;\
-    outline: none;\
-    background-color: #fff;\
-    cursor: pointer;\
-    margin-left: 50px;";
-
-DatepickerTemplates.closeButtonStyle = "\
-    cursor: pointer;"
-
-DatepickerTemplates.previousButtonStyle = "\
-    cursor: pointer;"
-
-DatepickerTemplates.nextButtonStyle = "\
-    cursor: pointer;"
-
-DatepickerTemplates.tableStyle = "\
-    font-size: 15px;\
-    background-color: #96deda;\
-    padding: 10px;\
-    text-align: center;\
-    cursor: default;\
-    border-radius: 10px;\
-    font-family: sans-serif;\
-    user-select: none;";
-
-DatepickerTemplates.tableHeadStyle = "\
-    font-size: 18px;\
-    font-weigth: 600;\
-    background-color: #96deda;\
-    text-align: center;";
-
-DatepickerTemplates.tableHeaderStyle = "\
-    color: #fff;";
-
-DatepickerTemplates.tableBodyStyle = "\
-    font-size: 20px;\
-    background-color: #D7FFFE;\
-    text-align: center;";
-
-DatepickerTemplates.tableHeadWeekendStyle = "\
-    color: red;";
-
-DatepickerTemplates.dateInputStyle = "\
-    display: inline-block;\
-    padding: 6px 15px;\
-    text-decoration: none;\
-    border: 2px solid #96deda;\
-    font-weight: 600;\
-    font-size: 14px;\
-    color: #000;\
-    border-radius: 20px;\
-    width: 80px;\
-    outline: none;\
-    background-color: #fff;";
 
 
 
