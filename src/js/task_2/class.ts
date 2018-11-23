ffd9e07a156f84af879974420d9d1df01a56c7ab
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-11-23 14:14:57 +0300

    task_2: modify class to ts

---
+++ src/js/task_2/class.ts
@@ -0,0 +1,53 @@
+class VehicleTs {
+
+    private _speed: Number;
+
+    constructor() {
+        this._speed = 0;
+    }
+
+    public getSpeed(): Number {
+        return this._speed;
+    }
+
+    public move(moveSpeed): Number {
+        return this._speed += moveSpeed;
+    }
+
+    public stop(): Number {
+        return this._speed = 0;
+    }
+
+    public vehicleHorn(): void {
+        alert('beep beep!');
+    }
+}
+
+class CargoVehicleTs extends VehicleTs {
+
+    private _loadCapacity: Number;
+    private _countOfGoods: Number;
+
+    constructor(loadCapacity: Number) {
+        super();
+        this._loadCapacity = loadCapacity;
+        this._countOfGoods = 0;
+    }
+
+    public loadGoods(goods: Number): void {
+        if (goods > this._loadCapacity) {
+            throw new Error("Превышена грузоподъемность = " + this._loadCapacity);
+        }
+
+        this._countOfGoods = goods;
+    }
+
+    public vehicleHorn(): void {
+        super.vehicleHorn();
+        alert("BEEP BEEP I'M JEEP!");
+    }
+
+    public get countOfGoods(): Number {
+        return this._countOfGoods;
+    }
+}
