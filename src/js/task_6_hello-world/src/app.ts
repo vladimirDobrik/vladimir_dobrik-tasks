2104b901c5f3790ec507f1350b208ab96e282534
vladimirDobrik <vladimir.d.6kg@gmail.com>
2018-12-17 17:22:00 +0300

    task_6_hello-world: implement task

---
+++ src/js/task_6_hello-world/src/app.ts
@@ -0,0 +1,8 @@
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
---
+++ src/js/task_6_hello-world/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CheckerPlugin} = require('awesome-typescript-loader');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = (env, options) => {
+    var isDevMode = options.mode !== 'production';
+
+    var config = {
+        entry: './src/app.ts',
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [{
+                    test: /\.ts$/,
+                    use: ['awesome-typescript-loader', 'angular2-template-loader'],
+                    exclude: [/\.(spec|e2e)\.ts$/]
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'raw-loader',
+                        'sass-loader'
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]'
+                        }
+                    }]
+                }
+            ]
+        },
+        plugins: [
+            new CheckerPlugin(),
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                filename: "./index.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ],
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx']
+        },
+        optimization: {
+            minimizer: [new UglifyJsPlugin()]
+        },
+        devServer: {
+            overlay: true,
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000
+        },
+        devtool: isDevMode
+            ? 'eval-source-map'
+            : false
+    }
+
+    return config;
+}
